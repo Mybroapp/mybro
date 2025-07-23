360ce7dc418ef269fa0d29d5a8d7d328f9905ed7
Mybroapp <juanantoniofulco@gmail.com>
2025-07-23 12:56:42 -0400

    Update ChatWindow.tsx

--- components/ChatWindow.tsx
+++ components/ChatWindow.tsx
@@ -121,7 +121,7 @@ export default function ChatWindow() {
 
       <div
         style={{
-          position: 'fixed',
+          position: 'sticky',
           bottom: 0,
           left: 0,
           right: 0,
